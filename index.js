590a85b2b11db2a1e1e663c5d9f7e37145fea297
Judyhouser80 <judyhouser80@gmail.com>
2019-08-11 15:36:08 -0400

    Commit today

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+document.querySelectorAll(".bounce")[i].addEventListener("mouseDown", function() {
 
+  var buttonInnerHTML = this.innerHTML;
+
+  makeSound(buttonInnerHTML);
+
+  buttonAnimation(buttonInnerHTML);
+
+});
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+makeSound(event.key);
+
+buttonAnimation(event.key);
+
+});
